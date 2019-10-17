66923819194f6b2d46c5c552222b712c5fc134e7
Julian <cusco2891@gmail.com>
2019-10-17 19:32:34 -0400

    activity src for templating/started inserting homepage

---
+++ src/components/pages/Blog.js
@@ -0,0 +1,10 @@
+import React from "react";
+import SearchResultContainer from "../SearchResultContainer";
+
+function Blog() {
+  return (
+    <SearchResultContainer/>
+  );
+}
+
+export default Blog;
