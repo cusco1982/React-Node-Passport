def157a6f20b610d72ddcd9267a1c080ab726b1e
Julian <cusco2891@gmail.com>
2019-10-22 19:43:27 -0400

    mongoose connection changed

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/propmanagedb");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/propmanagedb", {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
 
 // Start the API server
 app.listen(PORT, function() {
