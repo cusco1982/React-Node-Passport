5eb3f9b65c2a7fbde34fdba65ae838652b3e3501
Julian <cusco2891@gmail.com>
2019-10-24 18:59:46 -0400

    mongoDB connected//commented out Routes/tenants

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
-const routes = require("./routes");
+// const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 8080;
 require('dotenv').config();
@@ -14,15 +13,15 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-app.use(routes);
+// app.use(routes);
 
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/propmanagedb", {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => console.log('MongoDB Connected...'))
-    .catch(err => console.log(err));
-    
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/propmanagedb", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
