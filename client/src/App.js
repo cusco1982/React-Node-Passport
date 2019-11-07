d0754bc79ca8a8077469b3da10ab37594551f455
Julian <cusco2891@gmail.com>
2019-11-07 01:39:03 -0500

    appjs client

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Nav from "./components/Nav";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./pages/Main";
@@ -9,6 +9,15 @@ import Admin from "./pages/Admin";
 import Tenant from "./pages/Tenant";
 import Register from "./pages/Register";
 
+// ----------------------
+// import Profile from './components/Profile';
+// import Dashboard from './components/Dashboard';
+// import Login from './components/Login';
+// import Signup from './components/Signup';
+// import Home from './components/Home';
+// import NotFound from './components/NotFound';
+// ----------------------
+
 
 
 import "./css/style.css"
@@ -16,25 +25,28 @@ import "./css/style.css"
 
 
 
-function App() {
-  return (
-    <Router>
-      <div>
+export default class App extends Component {
+
+  render() {
+
+    return (
+
+      <Router>
         <Nav />
         <div className="container-fluid">
-          <Switch>
-            <Route exact path="/" component={Main} />
-            <Route exact path="/properties" component={Properties} />
-            <Route exact path="/Tenants" component={Tenants} />
-            <Route exact path="/Contact" component={Contacts} />
-            <Route exact path="/Admin" component={Admin} />
-            <Route exact path="/Tenant" component={Tenant} />
-            <Route exact path="/Register" component={Register} />
-          </Switch>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/properties" component={Properties} />
+          <Route exact path="/Tenants" component={Tenants} />
+          <Route exact path="/Contact" component={Contacts} />
+          <Route exact path="/Admin" component={Admin} />
+          <Route exact path="/Tenant" component={Tenant} />
+          <Route exact path="/Register" component={Register} />
+        </Switch>
         </div>
-      </div>
-    </Router>
-  );
+      </Router>
+
+    );
+  };
 }
 
-export default App;
